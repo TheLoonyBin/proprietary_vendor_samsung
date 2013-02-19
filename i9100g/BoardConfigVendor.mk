8dceccbb9c799c245d273e1b235fccf0e8128a30
Muhammed Nazim <mnm9994u@gmail.com>
2013-02-19 15:48:23 +0100

    Bad move...
    We Hate CM!
    
    Change-Id: I3d3866d96e0ab66ed257478468dbb48a296191cc

--- i9100g/BoardConfigVendor.mk
+++ i9100g/BoardConfigVendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,3 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+USE_CAMERA_STUB := false
+BOARD_USES_GENERIC_AUDIO := false
