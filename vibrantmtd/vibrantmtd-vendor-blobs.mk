ba000ba39f1e99b3e5cef81ace5b1e8496a61a82
Jrongi1 <jasonrongi1@gmail.com>
2013-09-22 00:08:47 -0400

    4.3

--- vibrantmtd/vibrantmtd-vendor-blobs.mk
+++ vibrantmtd/vibrantmtd-vendor-blobs.mk
@@ -20,13 +20,17 @@ PRODUCT_COPY_FILES := \
 # All the blobs necessary for vibrantmtd devices
 PRODUCT_COPY_FILES += \
     vendor/samsung/vibrantmtd/proprietary/gps.conf:system/etc/gps.conf \
+    vendor/samsung/vibrantmtd/proprietary/secgps.conf:system/etc/vendor/samsung-agps/secgps.conf \
     vendor/samsung/vibrantmtd/proprietary/gpsd:system/vendor/bin/gpsd \
     vendor/samsung/vibrantmtd/proprietary/libclientgps.so:system/lib/libclientgps.so \
     vendor/samsung/vibrantmtd/proprietary/libsamsung_hwlegacy.so:system/vendor/lib/libsamsung_hwlegacy.so \
+    vendor/samsung/vibrantmtd/proprietary/gps.xml:system/vendor/etc/gps.xml \
+    vendor/samsung/vibrantmtd/proprietary/gps.aries.so:system/vendor/lib/hw/gps.aries.so \
     vendor/samsung/vibrantmtd/proprietary/libril.so:system/lib/libril.so \
     vendor/samsung/vibrantmtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/vibrantmtd/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
     vendor/samsung/vibrantmtd/proprietary/rild:system/bin/rild \
     vendor/samsung/aries-common/proprietary/libsensor_yamaha_test.so:system/vendor/lib/libsensor_yamaha_test.so \
     vendor/samsung/aries-common/proprietary/orientationd:system/vendor/bin/orientationd \
-    vendor/samsung/aries-common/proprietary/geomagneticd:system/vendor/bin/geomagneticd
+    vendor/samsung/aries-common/proprietary/geomagneticd:system/vendor/bin/geomagneticd \
+    vendor/samsung/vibrantmtd/proprietary/Certification.cer:system/vendor/bin/Certification.cer
