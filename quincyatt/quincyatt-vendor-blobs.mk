62ea743f02421649a0913204e3f43a080e45ef65
David Ferguson <ferguson.david@gmail.com>
2014-05-02 13:44:48 -0400

    msm8660 devices: rename camera hw blob to make way for wrapper
      * wrapper is implemented in http://review.cyanogenmod.org/#/c/55132/
    
    Change-Id: I6a681c42ac9af86926f74f850e2b29bf4a7a373e

--- quincyatt/quincyatt-vendor-blobs.mk
+++ quincyatt/quincyatt-vendor-blobs.mk
@@ -84,7 +84,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/quincyatt/proprietary/lib/libcommondefs.so:system/lib/libcommondefs.so \
 	vendor/samsung/quincyatt/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so \
 	vendor/samsung/quincyatt/proprietary/lib/libakm.so:system/lib/libakm.so \
-	vendor/samsung/quincyatt/proprietary/lib/hw/camera.msm8660.so:system/lib/hw/camera.msm8660.so \
+	vendor/samsung/quincyatt/proprietary/lib/hw/camera.vendor.msm8660.so:system/lib/hw/camera.vendor.msm8660.so \
 	vendor/samsung/quincyatt/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
 	vendor/samsung/quincyatt/proprietary/lib/libgemini.so:system/lib/libgemini.so \
 	vendor/samsung/quincyatt/proprietary/lib/libs3cjpeg.so:system/lib/libs3cjpeg.so \
