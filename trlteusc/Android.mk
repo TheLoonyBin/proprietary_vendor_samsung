471444eeb79e02c863b141eaf82eb09b924494fe
nicklovell23 <nicklovell23@gmail.com>
2015-06-27 10:18:24 -0500

    Squash Update GPS libs to Lollipop and add GPS location service use tmo GPS blobs.

--- trlteusc/Android.mk
+++ trlteusc/Android.mk
@@ -17,16 +17,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),trlteusc)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
+# Leave this here for later.
 endif
 
