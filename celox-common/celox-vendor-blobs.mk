2f80db063c24fae6ccf792c03a70d07af4b8da1e
bryan2894 <bryan2894@gmail.com>
2015-12-02 00:06:22 -0500

    celox-common: build ril from source
    Conflicts:
    	celox-common/proprietary/bin/rild
    	celox-common/proprietary/lib/libril.so
    
    Change-Id: Idf4f31ad0777af3158a11488cbbde87ff70d3df3

--- celox-common/celox-vendor-blobs.mk
+++ celox-common/celox-vendor-blobs.mk
@@ -48,14 +48,12 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/celox-common/proprietary/bin/ks:system/bin/ks \
 	vendor/samsung/celox-common/proprietary/bin/qmuxd:system/bin/qmuxd \
 	vendor/samsung/celox-common/proprietary/bin/rmt_storage:system/bin/rmt_storage \
-	vendor/samsung/celox-common/proprietary/bin/rild:system/bin/rild \
 	vendor/samsung/celox-common/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
 	vendor/samsung/celox-common/proprietary/lib/libqmi.so:system/lib/libqmi.so \
 	vendor/samsung/celox-common/proprietary/lib/libqdi.so:system/lib/libqdi.so \
 	vendor/samsung/celox-common/proprietary/lib/libqdp.so:system/lib/libqdp.so \
 	vendor/samsung/celox-common/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
 	vendor/samsung/celox-common/proprietary/lib/libqueue.so:system/lib/libqueue.so \
-	vendor/samsung/celox-common/proprietary/lib/libril.so:system/lib/libril.so \
 	vendor/samsung/celox-common/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
 	vendor/samsung/celox-common/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
 	vendor/samsung/celox-common/proprietary/lib/libdsutils.so:system/lib/libdsutils.so \
