cc80fce2b089afb5ea00c2a2cc0a09d2c75e237f
=?UTF-8?q?D=2E=20Andrei=20M=C4=83ce=C8=99?= <dmaces@nd.edu>
2016-04-17 16:36:05 -0400

    omap4: Define VENDOR_SGX_CORE once & prepare symlinks
    Newer DDKs seem to expect different library names, so create sym-
    links for them, but do keep the blob names fully qualified. While
    at it, change module names to avoid defining VENDOR_SGX_CORE twice.
    
    The LOCAL_MODULE_STEM usage here is a bit awkward, but couldn't find
    a more elegant solution; LOCAL_MODULE_FILENAME seems to be NDK-specific.
    
    Also, when defined in BoardConfig, TARGET_BOARD_OMAP_CPU doesn't
    expand properly here. It would work if defined in common.mk, which
    was noticed with TARGET_BOARD_PLATFORM as well.

--- espresso-common/espresso-common-vendor.mk
+++ espresso-common/espresso-common-vendor.mk
@@ -14,23 +14,15 @@
 
 $(call inherit-product, vendor/samsung/espresso-common/common-vendor-blobs.mk)
 
-ifeq ($(TARGET_BOARD_OMAP_CPU),4470)
-	VENDOR_SGX_CORE := SGX544_112
-else ifeq ($(TARGET_BOARD_OMAP_CPU),4460)
-	VENDOR_SGX_CORE := SGX540_120
-else ifeq ($(TARGET_BOARD_OMAP_CPU),4430)
-	VENDOR_SGX_CORE := SGX540_120
-endif
-
 PRODUCT_PACKAGES += \
-	libglslcompiler_$(VENDOR_SGX_CORE) \
-	libIMGegl_$(VENDOR_SGX_CORE) \
-	libpvr2d_$(VENDOR_SGX_CORE) \
-	libpvrANDROID_WSEGL_$(VENDOR_SGX_CORE) \
-	libPVRScopeServices_$(VENDOR_SGX_CORE) \
-	libsrv_init_$(VENDOR_SGX_CORE) \
-	libsrv_um_$(VENDOR_SGX_CORE) \
-	libusc_$(VENDOR_SGX_CORE) \
-	libGLESv1_CM_POWERVR_$(VENDOR_SGX_CORE) \
-	libGLESv2_POWERVR_$(VENDOR_SGX_CORE) \
-	gralloc.omap$(TARGET_BOARD_OMAP_CPU)
+	libglslcompiler \
+	libIMGegl \
+	libpvr2d \
+	libpvrANDROID_WSEGL \
+	libPVRScopeServices \
+	libsrv_init \
+	libsrv_um \
+	libusc \
+	libGLESv1_CM_POWERVR \
+	libGLESv2_POWERVR \
+	gralloc.omap4
