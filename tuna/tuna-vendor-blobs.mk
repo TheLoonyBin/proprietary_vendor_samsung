9378241c4ed50e7d586441d41f035885fb28fc8e
=?UTF-8?q?D=2E=20Andrei=20M=C4=83ce=C8=99?= <dmaces@nd.edu>
2016-06-10 12:04:44 -0400

    widevine: Convert blob copying to modules w/ strip & pack relocs
    tuna: based on https://github.com/Unlegacy-Android/proprietary_vendor_widevine/commit/e43c16355bc75b553bf7d66b91883263a67d4204
    
    Change-Id: I517c9c75dc4d6619a57137fc502087156322f9a2

--- tuna/tuna-vendor-blobs.mk
+++ tuna/tuna-vendor-blobs.mk
@@ -6,14 +6,8 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/firmware/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin \
 	$(VENDOR_FOLDER)/firmware/smc_pa_wvdrm.ift:system/vendor/firmware/smc_pa_wvdrm.ift \
 	$(VENDOR_FOLDER)/etc/smc_normal_world_android_cfg.ini:system/vendor/etc/smc_normal_world_android_cfg.ini \
-	$(VENDOR_FOLDER)/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-	$(VENDOR_FOLDER)/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-	$(VENDOR_FOLDER)/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
 	$(VENDOR_FOLDER)/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
-	$(VENDOR_FOLDER)/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so \
-	$(VENDOR_FOLDER)/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-	$(VENDOR_FOLDER)/lib/libwvm.so:system/vendor/lib/libwvm.so \
-	$(VENDOR_FOLDER)/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
+	$(VENDOR_FOLDER)/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so
 
 # maguro-specific files copied to /system/vendor/maguro
 PRODUCT_COPY_FILES += \
