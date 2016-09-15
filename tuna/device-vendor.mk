ad668cc45ebab7c4c525cf8166a225cf198a9603
=?UTF-8?q?D=2E=20Andrei=20M=C4=83ce=C8=99?= <dmaces@nd.edu>
2016-09-15 07:53:06 -0400

    tuna: Update DRM blobs from Z3 (via serrano)
    Also, drop libdrmmtkutil as it doesn't seem to be needed anywhere.
    
    Change-Id: I7a8d5d2e2b5bfda7850fb70b20b916f166dcfcf4

--- tuna/device-vendor.mk
+++ tuna/device-vendor.mk
@@ -3,7 +3,6 @@ $(call inherit-product, vendor/samsung/tuna/tuna-vendor-blobs.mk)
 PRODUCT_PACKAGES += \
     libdrmwvmplugin \
     libwvdrmengine \
-    libdrmmtkutil \
     libwvdrm_L3 \
     libwvm \
     libWVStreamControlAPI_L3
