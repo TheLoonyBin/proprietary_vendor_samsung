0376e4cf8a1c4586c992bbaefc9726a957b01ff5
Andreas Blaesius <skate4life@gmx.de>
2017-01-17 14:06:10 +0100

    Switch to common ti vendor repo

--- espresso-common/espresso-common-vendor.mk
+++ espresso-common/espresso-common-vendor.mk
@@ -13,19 +13,3 @@
 # limitations under the License.
 
 $(call inherit-product, vendor/samsung/espresso-common/common-vendor-blobs.mk)
-
-VENDOR_SGX_CORE := \
-	SGX54$(if $(filter-out 4470,$(TARGET_BOARD_OMAP_CPU)),0_120,4_112)
-
-PRODUCT_PACKAGES += \
-	libglslcompiler \
-	libIMGegl \
-	libpvr2d \
-	libpvrANDROID_WSEGL \
-	libPVRScopeServices \
-	libsrv_init \
-	libsrv_um \
-	libusc \
-	libGLESv1_CM_POWERVR \
-	libGLESv2_POWERVR \
-	gralloc.omap4
